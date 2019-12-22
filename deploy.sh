a874e061ac96f730d7e39df8ce84cd99a0291498
stevanus <stevanus.mardiady@infracom-tech.com>
2019-12-22 13:30:20 +0700

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+python3 setup.py sdist bdist_wheel
+pip install --user --ignore-installed dist/*.whl
---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="pysparkprocessor-expecc",
+    version="0.0.1",
+    author="expecc",
+    author_email="expecc@expecc.com",
+    description="Common package for pyspark processing",
+    packages=setuptools.find_packages(),
+    include_package_data=True
+)
